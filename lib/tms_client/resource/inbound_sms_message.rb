9216538867e233f994c8451a6b478dee438889da
Benjamin Ortega <ben.ortega@gmail.com>
2013-04-01 11:53:29 -0500

    add sms keyword response to inbound sms

--- lib/tms_client/resource/inbound_sms_message.rb
+++ lib/tms_client/resource/inbound_sms_message.rb
@@ -3,6 +3,6 @@ module TMS #:nodoc:
     include InstanceResource
     
     # @!parse attr_reader :created_at, :completed_at, :from, :body, :to, :command_status
-    readonly_attributes :created_at, :completed_at, :from, :body, :to, :command_status
+    readonly_attributes :created_at, :completed_at, :from, :body, :to, :command_status, :keyword_response
   end
 end
