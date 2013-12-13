f7b0ea9b73640f5c55839f9e1956ccee806e6e04
Tyler Holien <tyler.holien@govdelivery.com>
2013-12-13 15:26:03 -0600

    Do not use ActiveSupport::Notifications
    Each instance of the TMS::Connection was getting added as an observer,
    causing multiple log messages when there was more than one instance. It
    would also have resulted in a memory leak for any long-lived process
    that creates multiple instances of TMS::Client.
    
    I think this was being done to get at the start and end time of the
    request so we can log the duration. I'm just doing it in the TMS::Logger
    middleware instead.

--- lib/tms_client/connection.rb
+++ lib/tms_client/connection.rb
@@ -22,21 +22,10 @@ class TMS::Connection
     self.connection = Faraday.new(:url => self.api_root) do |faraday|
       faraday.use TMS::Logger, self.logger if self.logger
       faraday.request :json
-      setup_logging(faraday)
       faraday.headers['X-AUTH-TOKEN'] = auth_token
       faraday.headers[:user_agent] = "GovDelivery Ruby TMS::Client #{TMS::VERSION}"
       faraday.response :json, :content_type => /\bjson$/
       faraday.adapter :net_http
-    end
-  end
-
-  def setup_logging(faraday)
-    faraday.use FaradayMiddleware::Instrumentation, {:name => 'tms_client'}
-    ActiveSupport::Notifications.subscribe('tms_client') do |name, starts, ends, _, env|
-      duration = ends - starts
-      logger.info "#{env[:method].to_s.upcase.ljust(7)}#{env[:status].to_s.ljust(4)}#{env[:url]} (#{duration} seconds)"
-      logger.debug('response headers') { JSON.pretty_generate env[:response_headers] }
-      logger.debug('response body') { env[:body] }
     end
   end
 
